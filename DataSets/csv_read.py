06fd2a2a327ddbfcfd56306675a911784a697ef0
Alex <aru5@cornell.com>
2017-10-24 02:57:25 -0400

    Added Reading and saving of Spectral Embedding and PCA modified data

--- DataSets/csv_read.py
+++ DataSets/csv_read.py
@@ -2,31 +2,48 @@ import numpy as np
 import os
 import pandas as pd
 from sklearn.decomposition import PCA
+from sklearn.manifold import SpectralEmbedding
 
 """
-    Visible functions:
-    load_extracted_features()
-    load_graph()
-    load_seed()
-    save_labels(str = fname, vals): Denote the filename you want as str
+    Quick CSV read function for data
+    Allows reads of preprocessed data, e.g PCA, SpectralEmbedding
+    For analysis of variance, you should run PCA on data in a separate script
+    To use this module in the main folder, get up an __init__.py file or use:
+
+    base = os.path.dirname(os.path.realpath(__file__))
+    sys.path.append(os.path.join(base,'DataSets'))
+
+    To set up the path
 """
 
 def unittests():
+    fnames = ['Seed_Similarity.csv', 'Seed.csv', 'Seed_Matrix.csv', 'Graph.csv', 'Graph_Matrix.csv',
+                'Extracted_features.csv', 'Extracted_features_PCA.csv',
+                'Extracted_features_PCA_Seeds.csv', 'SpectralEmbedding.csv']
     g = load_graph()
     g_s = load_graph(shape_match = True)
     e = load_extracted_features()
     s = load_seed()
+    s_m = load_seed_matrix()
     ss= load_seed_similarity()
-    assert g is not None, 'Missing Graph.csv'
-    assert g_s is not None, 'Missing Graph.csv, shape matched'
-    assert e is not None, 'Missing Extracted_features.csv'
-    assert s is not None, 'Missing Seed.csv'
-    assert g.shape == (7064950, 2), 'Graph has wrong size: {} should be (7064949, 2)'.format(g.shape)
-    assert g_s.shape == (6000, 6000), 'G_S has wrong size: {} should be (6000,6000)'.format(g_s.shape)
-    assert check_symmetric(g_s), 'G_S is not symmetric'
+    e_seeds = load_extracted_features(onlyseeds=True)
+    e_pca = load_extracted_features_PCA()
+    e_seeds_pca = load_extracted_features_PCA(onlyseeds=True)
+    spec = load_spectral_embedding()
+    for f in fnames:
+        assert os.path.isfile(f), "{} should be a file".format(f)
+    assert g.shape == (7064950, 2), 'Graph has wrong size: {} should be (7064950, 2)'.format(g.shape)
+    assert g_s.shape == (6000, 6000), 'Graph Similarity Matrix has wrong size: {} should be (6000,6000)'.format(g_s.shape)
+    assert s_m.shape == (60,60), 'Seed Matrix has wrong size: {} should be (60,60)'.format(s_m.shape)
+    assert check_symmetric(g_s), 'Graph Similarity Matrix is not symmetric'
+    assert check_symmetric(s_m), 'Seed Matrix is not symmetric'
     assert e.shape == (10000,1084), 'Extracted_features has wrong size: {} should be (10000,1084)'.format(e.shape)
     assert s.shape == (60,2), 'Seed has wrong size: {} should be (60,2)'.format(s.shape)
     assert ss.shape == (6000,10), 'Seed Similarity has wrong size: {} should be (6000,10)'.format(ss.shape)
+    assert e_seeds.shape == (60,1084), 'Extracted_features of seeds has wrong size: {} should be (60,1084)'.format(e_seeds.shape)
+    assert e_pca.shape == e.shape, 'Extracted_featuresPCA should match Extracted_features'
+    # assert spec.shape == g_s.shape, 'SpectralEmbedding should match Graph Matrix'
+
     print('Passed')
 
 # Load-csv is the general function.
@@ -85,20 +102,22 @@ def load_extracted_features(onlyseeds=False):
     return load_csv('Extracted_features.csv')
 
 # Loads extracted features with PCA selected k features
-def load_extracted_features_PCA(k=999, onlyseeds=False):
-    fname = "Extracted_features_PCA{}{}.csv".format(k, ('_Seeds' if onlyseeds else ''))
+# Uses a preloaded .csv of PCA run for all components
+def load_extracted_features_PCA(k=1084, onlyseeds=False):
+    fname = "Extracted_features_PCA{}.csv".format(('_Seeds' if onlyseeds else ''))
     preload = load_csv(fname)
-    if preload is not None:
-        return preload
-    else:
-        print("Running PCA on Extracted Features, keeping {} important components".format(k))
-        pca = PCA(n_components=k)
-        X = load_extracted_features( onlyseeds = onlyseeds )
-        X_new = pca.fit_transform(X)
-        print("New shape {}".format(X_new.shape))
-        assert X_new.shape == (X.shape[0],k), "New shape {} should be {}".format(X_new.shape,(X.shape[0],k))
-        np.savetxt(fname,  np.asarray(X_new), delimiter=",", fmt='%.4f')
-        return X_new
+    if preload is None:
+        print('Running PCA on extracted_features{}'.format('_Seeds' if onlyseeds else ''))
+        X = load_extracted_features(onlyseeds=onlyseeds)
+        features = min(X.shape[0], X.shape[1])
+        preload = PCA(n_components=features).fit_transform(X)
+        np.savetxt(fname, np.asarray(preload), delimiter=",", fmt='%.4f')
+        print('Saved')
+
+    true_k = min(k, preload.shape[1])
+    output = preload[:, :true_k] # Get the k first eigens of PCA output
+    assert output.shape == (preload.shape[0],true_k), "New shape {} should be {}".format(output.shape,(preload.shape[0],true_k))
+    return output
 
 def load_seed():
     return np.array(load_csv('Seed.csv'), dtype='int')
@@ -109,6 +128,7 @@ def load_seed_matrix(fname='Seed_Matrix'):
     if preload is not None:
         return preload
     else:
+        print('Creating Seed Matrix')
         s = load_seed()
         nums = s.shape[0]
         output = np.zeros((nums, nums), dtype='int')
@@ -118,7 +138,27 @@ def load_seed_matrix(fname='Seed_Matrix'):
                 output[i,j] = int(s[i,1]==s[j,1])
         assert check_symmetric(output), 'Seed Matrix should be symmetric'
         np.savetxt(fname+'.csv',  np.asarray(output), delimiter=",", fmt='%d')
+        print('Done')
         return output
+
+def load_spectral_embedding(k=5990):
+    fname = 'SpectralEmbedding.csv'
+    preload = load_csv(fname)
+    if preload is None:
+        print('Run spectralembedding on seed_matrix')
+        matrix = load_graph(shape_match=True)
+        features = 5990
+        # I reduced the number of feature to 5990 to handle an issue with
+        # using Sklearn Spectral embedding with n_components = matrix size
+        # Issue is with eigsh
+        preload = SpectralEmbedding(n_components=features).fit_transform(matrix)
+        np.savetxt(fname, np.asarray(preload), delimiter=",", fmt='%.4f')
+        print('Saved')
+
+    true_k = min(k, preload.shape[1])
+    output = preload[:, :true_k] # Get the k first eigens of PCA output
+    assert output.shape == (preload.shape[0],true_k), "New shape {} should be {}".format(output.shape,(preload.shape[0],true_k))
+    return output
 
 # loads (or creates) a .csv for similarities to all known labels as a [10] element array per row
 # e.g [0] : [sim to 0s, sim to 1s,.... sim to 9s]
@@ -154,16 +194,18 @@ def load_seed_similarity(fname='Seed_Similarity'):
 
 if __name__ == '__main__':
     print("Visible functions")
-    print("load_extracted_features()    : Load Extracted_features.csv as a numpy array")
-    print("load_graph(shape_match=False): Load Graph.csv as a numpy array")
-    print("                               shape_match will make it a 6000x6000 matrix")
-    print("load_seed()                  : Load Seed.csv into a numpy array")
-    print("load_seed_similarity()       : Load Seed_Similarity.csv into a numpy array")
-    print("save_labels(fname,vals)      : Denote the filename you want as str.")
-    print("                               Doesn't need to include .csv in fname")
-    print("                               Will be saved into Labels/fname")
+    print("load_extracted_features(onlyseeds=False)  : Load Extracted_features.csv as a numpy array")
+    print("                                            onlyseeds will load only features of seeds")
+    print("load_graph(shape_match=False)             : Load Graph.csv as a numpy array")
+    print("                                            shape_match will make it a 6000x6000 adjacency matrix")
+    print("                                            A point(i,j) = 1 if element i and j are similar")
+    print("load_seed()                               : Load Seed.csv into a numpy array")
+    print("load_seed_similarity()                    : Load Seed_Similarity.csv into a numpy array")
+    print("load_seed_matrix()                        : Loads an adjacency matrix of Seeds")
+    print("                                            A point(i,j) = 1 if Seed i and Seed j are the same label")
+    print("load_extracted_features_PCA(k, onlyseeds) : Loads the PCA output of Extracted_features.csv")
+    print("Default k = 1084, onlyseeds = False         with k components. onlyseeds is also an allowed arg")
+    print("load_spectral_embedding(k)                : Loads the SpectralEmbedding of load_graph(shape_match=True)")
+    print("                                            with k components")
     print("Running Unit Tests (This will take a while): ...")
-    # load_extracted_features_PCA(k=999)
-    load_seed_matrix()
-    load_extracted_features_PCA(k=32, onlyseeds=True)
-    # unittests()
+    unittests()
