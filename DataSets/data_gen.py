38b0f9748fff3203de29fb3ba9105e8726700d9a
Alex-Ueki <aru5@cornell.edu>
2017-10-27 16:49:27 -0400

    Updates to models.py, now training with CCA data

--- DataSets/data_gen.py
+++ DataSets/data_gen.py
@@ -1,5 +1,5 @@
 import numpy as np
-from csv_read import load_extracted_features, load_graph, load_seed, load_seed_matrix, load_extracted_features_PCA
+from csv_read import load_extracted_features, load_graph, load_seed, load_seed_matrix, load_extracted_features_PCA, load_CCA_features
 
 class DataGen():
     # Training is true if for training, else it is for validation/evaluation/prediction
@@ -99,7 +99,9 @@ def generate_siamese_validation_data(nums=880, input_shape=(1,1084)):
 
 def seed_generator(batch_size=128,input_shape=(1,1084)):
     g = load_seed_matrix()
-    f = load_extracted_features_PCA(k=input_shape[0]*input_shape[1], onlyseeds=True)
+    f = load_CCA_features(k = input_shape[0]*input_shape[1], onlyseeds=True)
+    if f is None:
+        print('You must compute CCA for {} features'.format(f))
     nums = f.shape[0]
     input1 = np.empty(
         (batch_size, ) + input_shape)
@@ -118,7 +120,9 @@ def seed_generator(batch_size=128,input_shape=(1,1084)):
 
 def seed_validation(input_shape=(1,1084)):
     g = load_seed_matrix()
-    f = load_extracted_features_PCA(k=input_shape[0]*input_shape[1], onlyseeds=True)
+    f = load_CCA_features(k = input_shape[0]*input_shape[1], onlyseeds=True)
+    if f is None:
+        print('You must compute CCA for {} features'.format(f))
     nums = f.shape[0]
     input1 = np.empty(
         (nums**2,  ) + input_shape )
@@ -132,4 +136,19 @@ def seed_validation(input_shape=(1,1084)):
             input1[batch_index], input2[batch_index] = f[i] , f[j]
             outputs[batch_index] = g[i, j]
             batch_index += 1
+    return [input1, input2] ,outputs
+
+# runs a sanity check on a siamese need, ie a = a ==> 1
+def siamese_sanity_check(input_shape=(1,1084)):
+    f = load_CCA_features(k = input_shape[0]*input_shape[1])
+    nums = f.shape[0]
+    input1 = np.empty(
+        (nums,  ) + input_shape )
+    input2 = np.empty(
+        (nums,  ) + input_shape )
+    outputs = np.empty(
+        (nums, ) )
+    for i in range(nums):
+        input1[i], input2[i] = f[i] , f[i]
+        outputs[i] = 1
     return [input1, input2] ,outputs
--- models.py
+++ models.py
@@ -9,37 +9,36 @@ sys.path.append(os.path.join(base,'Modules'))
 # https://sorenbouma.github.io/blog/oneshot/
 import numpy as np
 import numpy.random as rng
-from keras.layers import Input, Conv2D, Lambda, merge, subtract, Dense, Flatten, Dropout
+from keras.layers import Input, Conv2D, Lambda, subtract, Dense, Flatten, Dropout, GaussianNoise
 from keras.models import Model, Sequential
 from keras.regularizers import l2
 from keras import backend as K
 from keras.optimizers import SGD,Adam
 from keras.losses import binary_crossentropy
-from data_gen import generate_siamese_validation_data, siamese_training_data_generator
-
-def W_init(shape,name=None):
-    """Initialize weights as in paper"""
-    values = rng.normal(loc=0,scale=1e-2,size=shape)
-    return K.variable(values,name=name)
-#//TODO: figure out how to initialize layer biases in keras.
-def b_init(shape,name=None):
-    """Initialize bias as in paper"""
-    values=rng.normal(loc=0.5,scale=1e-2,size=shape)
-    return K.variable(values,name=name)
+from data_gen import seed_generator, seed_validation, siamese_sanity_check
 
 # x_train, y_train = generate_training_data(nums=880)
 # x_test, y_test = generate_validation_data()
-input_shape = (1, 1084)
+k = 8
+input_shape = (1, k)
 left_input = Input(input_shape)
 right_input = Input(input_shape)
-#build convnet to use in each siamese 'leg'
+
+# Internal model is a softmax 10-class classifier
 model = Sequential()
-model.add(Dense(64, input_shape=input_shape, activation='relu'))#, kernel_regularizer=l2(2e-4),kernel_initializer=W_init,bias_initializer=b_init))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))#,kernel_regularizer=l2(2e-4),kernel_initializer=W_init,bias_initializer=b_init))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='sigmoid'))#, kernel_regularizer=l2(2e-4),kernel_initializer=W_init,bias_initializer=b_init))
+# model.add(Dense(64, input_shape=input_shape, activation='relu'))
+# model.add(Flatten())
+model.add(GaussianNoise(0.001, input_shape=input_shape))
 model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
 #encode each of the two inputs into a vector with the convnet
 encoded_l, encoded_r = model(left_input), model(right_input)
 #merge two encoded inputs with the l1 distance between them
@@ -60,12 +59,20 @@ siamese_net.compile(loss='binary_crossentropy',
 # model.fit(x_train, y_train,
 #           epochs=10,
 #           batch_size=128)
+override = False
+if os.path.isfile('test.hdf5') and not override:
+    siamese_net.load_weights('test.hdf5')
+else:
+    steps = 10000
+    siamese_net.fit_generator(seed_generator(batch_size=batch_size, input_shape=input_shape),
+                            steps, epochs=5,
+                            validation_data=seed_validation(input_shape=input_shape))
 
-nums = 5120
-steps = (nums**2)//batch_size
-siamese_net.fit_generator(siamese_training_data_generator(nums = nums, input_shape=input_shape),
-                        steps, epochs=5,
-                        validation_data=generate_siamese_validation_data(input_shape=input_shape)
-                        )
+x_test, y_test = siamese_sanity_check(input_shape=input_shape)
+sanity_check = siamese_net.evaluate(x_test, y_test, batch_size=10)
+print('Final Score : {}'.format(sanity_check))
 
-print('Final Score : {}'.format(model.evaluate(x_test, y_test, batch_size=16)))
+if sanity_check[1] < 1.0:
+    print('Siamese Net isn\'t working')
+elif not os.path.isfile('test.hdf5') or override:
+    siamese_net.save_weights('test.hdf5')
